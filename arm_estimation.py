e530f048450ffbb3984166f59603faafd3deec12
DDLAK <DDLAK50@163.com>
2023-04-18 21:51:17 +0800

    Add initial source code

---
+++ arm_estimation.py
@@ -0,0 +1,13 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+# Step 1: Input a default image for configuration to find the original proportions.
+
+# Step 2: Find the real-time proportion.
+
+# Step 3: Estimate the 3D coordinates of points of arms.
