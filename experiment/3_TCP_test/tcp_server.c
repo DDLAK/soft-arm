878f143e1fd6f9ae722e02f1b75df3615d9b53c4
jinru wu <67178619+DDLAK@users.noreply.github.com>
2023-07-26 15:44:37 +0100

    Edit tcp_server.c (#7)

--- experiment/3_TCP_test/tcp_server.c
+++ experiment/3_TCP_test/tcp_server.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define SERVER_PORT 8888
-#define LISTEN_BACKLOG 10
+#define LISTEN_BACKLOG 1	// Only permit one terminal to control the robot.
+#define BUFFER_SIZE 3+1		// Receive 3 same numbers, to ensure the instruction is correct.
 
 int main (int argc, char* argv[])
 {
 	int server_socket, client_socket;
 	struct sockaddr_in server_addr, client_addr;
 	int addr_len;
+	unsigned char buffer[BUFFER_SIZE];
+	int recv_len;
 
 	// Create a socket for tcp server.
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket == -1)
+	if (-1 == server_socket)
 	{
 		perror("Socket");
 		exit(EXIT_FAILURE);
@@ -29,15 +33,15 @@ int main (int argc, char* argv[])
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	memset(server_addr.sin_zero, '\0', 8);
 	
-	if (bind(server_socket, (const struct sockaddr *)&server_addr, 
-			sizeof(server_addr)) == -1)
+	if (-1 == bind(server_socket, (const struct sockaddr *)&server_addr, 
+			sizeof(server_addr)))
 	{
 		perror("Bind");
 		exit(EXIT_FAILURE);
 	}
 
 	// Start to listen.
-	if (listen(server_socket, LISTEN_BACKLOG) == -1)
+	if (-1 == listen(server_socket, LISTEN_BACKLOG))
 	{
 		perror("Listen");
 		exit(EXIT_FAILURE);
@@ -45,27 +49,33 @@ int main (int argc, char* argv[])
 	
 	// Wait for accepting a connection.
 	addr_len = sizeof(struct sockaddr);
-	int accept_return;
-	while (1)
+	client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
+	if (client_socket != -1)
 	{
-		accept_return = accept(server_socket, (struct sockaddr *)&client_addr,
-				&addr_len);
-		printf("%d\n", accept_return);
-		if (accept_return != -1)
-		{
-			printf("Successful\n");
-			printf("Successful connect to: %s\n",
-					inet_ntoa(client_addr.sin_addr));
+		printf("Successful connect to: %d %s\n",
+				client_socket, inet_ntoa(client_addr.sin_addr));
+	}
+	else
+	{
+		perror("Accept");
+	}
 
+	// Send / Receive data.
+	while(0 != strcmp(buffer, "end"))
+	{
+		recv_len = recv(client_socket, &buffer, BUFFER_SIZE-1, 0);
+		if (-1 == recv_len)
+		{	
+			perror("Receive");
+			close(client_socket);
+			exit(EXIT_FAILURE);
 		}
 		else
 		{
-			perror("Accept");
+			buffer[recv_len] = '\0';
+			printf("Received message: %s\n", buffer);
 		}
 	}
-
-
-	// Send / Receive data.
-	
+		
 	return 0;
 }
