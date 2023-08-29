d4b9bf466d54fb1cfa532aeadb1f7373879f3253
DDLAK <DDLAK50@163.com>
2023-08-29 22:30:48 +0100

    Modify on laptop_end

--- experiment/6_Filter/analysis/analysis_test2.py
+++ experiment/6_Filter/analysis/analysis_test2.py
@@ -15,7 +15,7 @@ plt.plot(x, list(estimated_data['esti_x']), label='x')
 plt.plot(x, list(estimated_data['esti_y']), label='y')
 
 plt.plot(x, list(estimated_data['esti_z']), label='z')
-aver_z = np.ones(shape=2000) * np.average(estimated_data['esti_z'])
+aver_z = np.ones(shape=6000) * np.average(estimated_data['esti_z'])
 plt.plot(x, aver_z, 'black')
 
 plt.legend()
