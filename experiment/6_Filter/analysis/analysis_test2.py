97f449fd027f1d1be5591469457106351f1ad46b
DDLAK <DDLAK50@163.com>
2023-08-29 22:54:24 +0100

    Modify on laptop_end

--- experiment/6_Filter/analysis/analysis_test2.py
+++ experiment/6_Filter/analysis/analysis_test2.py
@@ -10,12 +10,12 @@ fig = plt.figure()
 
 # 折线图
 x = estimated_data['time']
-plt.plot(x, list(estimated_data['esti_x']), label='x')
+plt.plot(x, list(estimated_data['an_x']), label='x')
 
-plt.plot(x, list(estimated_data['esti_y']), label='y')
+plt.plot(x, list(estimated_data['an_y']), label='y')
 
-plt.plot(x, list(estimated_data['esti_z']), label='z')
-aver_z = np.ones(shape=6000) * np.average(estimated_data['esti_z'])
+plt.plot(x, list(estimated_data['an_z']), label='z')
+aver_z = np.ones(shape=6000) * np.average(estimated_data['an_z'])
 plt.plot(x, aver_z, 'black')
 
 plt.legend()
@@ -26,11 +26,11 @@ fig = plt.figure()
 
 # 折线图
 x = estimated_data['time']
-plt.plot(x, list(estimated_data['esti_a']), label='x')
+plt.plot(x, list(estimated_data['wn_x']), label='x')
 
-plt.plot(x, list(estimated_data['esti_b']), label='y')
+plt.plot(x, list(estimated_data['wn_y']), label='y')
 
-plt.plot(x, list(estimated_data['esti_c']), label='z')
+plt.plot(x, list(estimated_data['wn_z']), label='z')
 
 plt.legend()
 plt.show()
